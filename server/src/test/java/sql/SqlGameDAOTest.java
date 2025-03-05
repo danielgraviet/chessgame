be935c4f76fbc30a491ec4de5b4d0eea07d7889d
danielgraviet <dgravs5@gmail.com>
2025-03-05 13:40:31 -0700

    added some sqlgamedao tests

--- server/src/test/java/sql/SqlGameDAOTest.java
+++ server/src/test/java/sql/SqlGameDAOTest.java
@@ -4,6 +4,7 @@ import dataaccess.DataAccessException;
 import dataaccess.SqlGameDAO;
 import model.game.GameData;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,6 +25,7 @@ public class SqlGameDAOTest {
     }
 
     @Test
+    @Order(1)
     @DisplayName("Create Game Successfully")
     void testCreateGameSuccess() throws DataAccessException {
         int gameId = gameDAO.createGame(authToken,"Test Game");
@@ -36,4 +38,58 @@ public class SqlGameDAOTest {
         assertNull(game.whiteUsername(), "White username should be null initially");
         assertNull(game.blackUsername(), "Black username should be null initially");
     }
+
+    // get game by id test
+    @Test
+    @Order(2)
+    @DisplayName("Get Game ID Successfully")
+    void testGetGameByID() throws DataAccessException {
+        // create a game
+        String gameName = "Test Game";
+        int gameID = gameDAO.createGame(authToken, gameName);
+
+        // check the gameID is positive
+        assertTrue(gameID > 0, "Game ID should be positive");
+
+        // get the game by ID
+        GameData retrievedGame = gameDAO.getGameByID(gameID);
+        assertEquals(gameID, retrievedGame.gameID(), "Game ID should match");
+        assertEquals(gameName, retrievedGame.gameName(), "Game name should match");
+    }
+
+    // update game test
+    @Test
+    @Order(3)
+    @DisplayName("Update game successfully")
+    void testUpdateGame() throws DataAccessException {
+        // create a game
+        String gameName = "Test Game";
+        int gameID = gameDAO.createGame(authToken, gameName);
+
+        // check the gameID is positive
+        assertTrue(gameID > 0, "Game ID should be positive");
+
+        // get original game data
+        GameData originalGame = gameDAO.getGameByID(gameID);
+
+        // update game
+        String updatedGameName = "Updated Game";
+        GameData updatedGameData = new GameData(
+                gameID,
+                "WhiteUser",
+                "BlackUser",
+                updatedGameName,
+                originalGame.game());
+        gameDAO.updateGame(updatedGameData);
+
+        // check for success
+        assertNotSame(originalGame.whiteUsername(), updatedGameData.whiteUsername(), "white username should not match");
+        assertNotSame(originalGame.blackUsername(), updatedGameData.blackUsername(), "black username should not match");
+        assertNotSame(originalGame.gameName(), updatedGameData.gameName(), "game name should not match");
+        assertEquals(originalGame.game(), updatedGameData.game(), "Games should match");
+    }
+
+    // get all games test
+
+    // clear test
 }
